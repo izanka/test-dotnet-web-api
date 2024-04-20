020e32a854fcdd948be79e896a0867f19500b992
izanka <42039017+izanka@users.noreply.github.com>
2024-04-20 12:30:29 +0530

    Added files from initial project setup

---
+++ TestWebAPI/Controllers/DataController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestWebAPI.Models;
+
+namespace TestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // [EnableCors]
+    public class DataController : ControllerBase
+    {
+        [HttpPost, Route("processData")]
+        public IActionResult ProcessData([FromBody] DataModel data) 
+        {
+            // Process the data (perform some action)
+            var result = "Hello, " + data.Name;
+
+            // Return the result
+            return Ok( new { result });
+        }
+    }
+}
---
+++ TestWebAPI/Models/DataModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestWebAPI.Models
+{
+    public class DataModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
